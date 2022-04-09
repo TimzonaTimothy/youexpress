222f0e90f8cd6d02842cb04ce71a1f0841620785
Arinze Timothy <78275394+TimzonaTimothy@users.noreply.github.com>
2022-04-09 13:36:49 +0100

    database

--- CMS/settings.py
+++ CMS/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'CMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
